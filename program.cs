952285cca6945a5fe45031ca7ce30567309bf9ac
Vydzi <101144771+Vydzi@users.noreply.github.com>
2022-09-12 08:00:29 +0200

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,16 @@
+<Button Name="btn1" Background="Pink" 
+        BorderBrush="Black" BorderThickness="1" 
+        Click="OnClick1" ClickMode="Hover">
+  ClickMe1
+</Button>
+
+<Button Name="btn2" Background="LightBlue" 
+        BorderBrush="Black" BorderThickness="1" 
+        Click="OnClick2" ClickMode="Press">
+  ClickMe2
+</Button>
+
+<Button Name="btn3" 
+        Click="OnClick3" ClickMode="Release">
+  Reset
+</Button>
